5adbf807a855d48a3adca5f4a94c6ba347f8216a
Simon Svensson <sisve@devhost.se>
2020-05-23 20:30:12 +0200

    Removed references to non-existing tests

--- tests/Commands/GettextCommandTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class GettextCommandTest extends \PHPUnit_Framework_TestCase {
-	public function testGettextCommand() {
-		
-	}
-}
--- tests/GettextTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-class GettextTest extends \PHPUnit_Framework_TestCase {
-	public function testGettext() {
-
-	}
-}
